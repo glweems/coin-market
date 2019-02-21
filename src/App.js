7b3ae245cad195ef7a2ba0b9a4a8fd1a8739d273
Garrett Weems <gwgraphicdesign@gmail.com>
2019-02-21 14:23:58 -0500

    resolve

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import CoinTable from "./Components/CoinTable";
-import Hero from "./Components/Hero";
+import CoinTable from "./Components/CoinTable/CoinTable";
+import Hero from "./Components/Hero/Hero";
 import { CoinPage } from "./CoinPage";
 
 const HomePage = () => (
