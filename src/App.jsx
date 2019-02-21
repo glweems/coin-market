abbd1e33db5f53d526c9f4be205477c3dc6127bc
Garrett Weems <gwgraphicdesign@gmail.com>
2019-02-21 12:50:55 -0500

    restructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,85 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import CoinTable from "./Components/CoinTable";
 import Hero from "./Components/Hero";
-class App extends Component {
+import axios from "axios";
+
+const HomePage = () => (
+  <React.Fragment>
+    <section className="container is-fluid">
+      <Hero title="Coin Markets" />
+    </section>
+    <section className="container">
+      <CoinTable />
+    </section>
+  </React.Fragment>
+);
+
+class CoinPage extends Component {
+  state = {};
+  COIN_ID = this.props.match.params.id;
+
+  // * Get Coin Info
+  async getCoinInfo() {
+    const res = await axios(
+      `https://min-api.cryptocompare.com/data/coin/generalinfo?fsyms=${
+        this.COIN_ID
+      }&tsym=USD`
+    );
+    return await { ...res.data.Data[0] };
+  }
+
+  // * Get Coin Stats
+  async getStats() {
+    const res = await axios(
+      `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${
+        this.COIN_ID
+      }&tsyms=USD,BTC`
+    );
+    return await { ...res.data.DISPLAY[this.COIN_ID] };
+  }
+  componentDidMount() {
+    if (!this.state.data) {
+      (async () => {
+        try {
+          this.setState({
+            info: { ...(await this.getCoinInfo()).CoinInfo },
+            usd: { ...(await this.getStats()).USD },
+            btc: { ...(await this.getStats()).BTC }
+          });
+          this.setState({ data: await this.getStats() });
+        } catch (error) {
+          console.error(error);
+        }
+      })();
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <section className="container" style={AppStyle}>
-          <Hero title="markets" />
-          <CoinTable />
+      <React.Fragment>
+        <Hero title={this.props.match.params.id} />
+        <section>
+          <p>{this.COIN_ID}</p>
+          {!this.state.data ? <em>Loading...</em> : <p>{this.COIN_ID}</p>}
         </section>
-      </div>
+      </React.Fragment>
     );
   }
 }
 
-const AppStyle = {
-  maxWidth: "100%"
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <React.Fragment>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/:id" component={CoinPage} />
+          </React.Fragment>
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
-import TableRow from "./TableRow";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 export class CoinTable extends Component {
   state = {};
-  async getData() {
+
+  async getCoins() {
     const res = await axios(
       "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD"
     );
-    return await res.data.Data; // (Or whatever)
+    return await res.data.Data;
   }
-  constructor(...args) {
-    super(...args);
-    this.state = { data: null };
-  }
+
   componentDidMount() {
+    let data = this.getCoins();
     if (!this.state.data) {
       (async () => {
         try {
-          this.setState({ data: await this.getData() });
+          this.setState({
+            coins: [
+              ...(await data).map(coin => {
+                return {
+                  CoinInfo: { ...{ ...coin.CoinInfo }, ...coin.DISPLAY.USD }
+                };
+              })
+            ],
+            data: await data
+          });
         } catch (error) {
           console.error(error);
         }
@@ -26,41 +34,81 @@ export class CoinTable extends Component {
     }
   }
   render() {
-    const headers = ["Rank", "Name", "Market Cap", "Price", "Change"];
-    const TableHeader = () => (
-      <thead>
-        <tr>
-          {headers.map(header => (
-            <th key={header}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-    );
-
     const TableRows = () =>
       this.state.data.map((row, index) => (
         <TableRow key={row.CoinInfo.Name} coin={row} rank={index} />
       ));
 
-    const table = () => (
-      <table className="table is-fullwidth is-striped" style={tableStyle}>
-        {TableHeader()}
+    const Table = () => (
+      <table className="table is-striped" style={tableStyle}>
+        {TableHeader(TableHeaders)}
         <tbody>{TableRows()}</tbody>
       </table>
     );
     return (
-      <section>
-        <div className="container is-fluid">
-          {!this.state.data ? <em>Loading...</em> : table()}
-        </div>
-      </section>
+      <section>{!this.state.data ? <em>Loading...</em> : Table()}</section>
     );
   }
 }
 
+const TableHeaders = ["Rank", "Name", "Market Cap", "Price", "Change"];
+
+// * Table Header
+const TableHeader = headers => (
+  <thead>
+    <tr>
+      {headers.map(header => (
+        <th key={header}>{header}</th>
+      ))}
+    </tr>
+  </thead>
+);
+
+// * Table Row
+const TableRow = props => {
+  const changeStyle = () => {
+    return {
+      color: props.coin.DISPLAY.USD.CHANGEPCTDAY >= 0 ? "green" : "red"
+    };
+  };
+
+  const imgStyle = {
+    width: "25px",
+    verticalAlign: "middle",
+    marginRight: "1rem"
+  };
+
+  const { coin, rank } = props;
+  const { Name } = coin.CoinInfo;
+  const { MKTCAP, PRICE, CHANGEPCTDAY, IMAGEURL } = coin.DISPLAY.USD;
+  const CoinImg = URL => `https://cryptocompare.com/${URL}`;
+
+  const NameRow = () => (
+    <Link to={Name}>
+      <img style={imgStyle} src={CoinImg(IMAGEURL)} alt={`${Name}`} />
+      {Name}
+    </Link>
+  );
+
+  return (
+    <tr>
+      <td>{rank}</td>
+      <td style={nameCell}>{NameRow()}</td>
+      <td>{MKTCAP}</td>
+      <td>{PRICE}</td>
+      <td style={changeStyle()}>{`${CHANGEPCTDAY}%`}</td>
+    </tr>
+  );
+};
+
+const nameCell = {
+  fontWeight: "500"
+};
+
 const tableStyle = {
-  maxWidth: "100%",
-  overflow: "scroll"
+  width: "100%",
+  overflow: "scroll",
+  margin: "0 auto"
 };
 
 export default CoinTable;
