859526bb0ee1e10b4ef014befb9114ba6c76346a
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-02 09:58:17 -0500

    pagination

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,76 +1,64 @@
-import React, { Component } from 'react';
-import { Line } from 'react-chartjs-2';
-import axios from 'axios';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Line } from "react-chartjs-2";
+import { apiFetch, coinChartUrl } from "../Api";
+import LoadingSpinner from "./LoadingSpinner";
 
 export class CoinChart extends Component {
-  COIN_ID = this.props.COIN_ID;
-
-  // * Get Coin Chart Data
-  async getChartData() {
-    const res = await axios(
-      `https://min-api.cryptocompare.com/data/histoday?fsym=${
-        this.COIN_ID
-      }&tsym=USD&limit=10`
-    );
-    return await res.data.Data;
-  }
+  state = {
+    COIN_ID: this.props.COIN_ID
+  };
 
   componentDidMount() {
-    if (!this.state) {
-      (async () => {
-        try {
-          this.setState({
-            data: await this.getChartData(),
-          });
-        } catch (error) {
-          console.error(error);
+    apiFetch(coinChartUrl(this.state.COIN_ID, 10), data => {
+      console.log(`coinChartUrl Fetch Successful`);
+      this.setState({
+        ChartData: {
+          labels: data.Data.map(point => Date.now(point.time)),
+          datasets: [
+            {
+              label: this.state.COIN_ID,
+              fill: true,
+              lineTension: 0.1,
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              borderCapStyle: "butt",
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: "miter",
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: [...data.Data.map(point => point.high)]
+            }
+          ]
         }
-      })();
-    }
+      });
+    });
   }
 
   render() {
     return (
       <React.Fragment>
-        {!this.state ? (
-          <em>Loading..</em>
+        {!this.state.ChartData ? (
+          <LoadingSpinner />
         ) : (
-          <section className="container">
-            <Line
-              data={{
-                labels: this.state.data.map(data =>
-                  new Date().getMinutes(data.time)
-                ),
-                datasets: [
-                  {
-                    label: this.props.COIN_ID,
-                    fill: true,
-                    lineTension: 0.1,
-                    backgroundColor: 'rgba(75,192,192,0.4)',
-                    borderColor: 'rgba(75,192,192,1)',
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: 'rgba(75,192,192,1)',
-                    pointBackgroundColor: '#fff',
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                    pointHoverBorderColor: 'rgba(220,220,220,1)',
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 10,
-                    data: this.state.data.map(data => data.high),
-                  },
-                ],
-              }}
-            />
-          </section>
+          <Line data={this.state.ChartData} />
         )}
       </React.Fragment>
     );
   }
 }
+
+// Prop Types
+CoinChart.propTypes = {
+  COIN_ID: PropTypes.string
+};
 
 export default CoinChart;
